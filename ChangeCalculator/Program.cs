aeb610775ab76854434fd8a89e91858203cb5b22
jakebostic <jake_bostic@yahoo.com>
2018-12-19 13:38:49 -0500

    Initial Commit

---
+++ ChangeCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ChangeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Change Calculator");
+            Console.WriteLine();
+
+            Console.Write("Enter number of cents (0-99): ");
+            if (int.TryParse(Console.ReadLine(), out int cents))
+            {
+                Console.WriteLine();
+
+                int quarters = cents / 25;
+                cents %= 25;
+                int dimes = cents / 10;
+                cents %= 10;
+                int nickels = cents / 5;
+                int pennies = cents % 5;
+
+                Console.WriteLine("Quarters: " + quarters);
+                Console.WriteLine("Dimes:    " + dimes);
+                Console.WriteLine("Nickels:  " + nickels);
+                Console.WriteLine("Pennies:  " + pennies);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+        }
+    }
+}
